292b59b1700a02f3df22149d123a678d78235224
Ahmed-Z <53438617+Ahmed-Z@users.noreply.github.com>
2020-05-06 03:11:53 +0200

    remove request

--- arpscan.py
+++ arpscan.py
@@ -1,11 +1,10 @@
 from scapy.all import *
-import subprocess,requests
+import subprocess
 
-class arpScan:
+class Arpscan:
     def __init__(self):
-        cmd = subprocess.check_output('ip r | grep default',shell = True, stderr=None)
-        result = cmd.split()
-        self.gateway = str(result[2].decode('utf-8'))
+        cmd = subprocess.check_output('ip r | grep default',shell = True, stderr=None).split()
+        self.gateway = str(cmd[2].decode('utf-8'))
     
     def get_mac(self,mac):
         mac = mac.upper().replace(':','')[0:6]
@@ -19,7 +18,7 @@ class arpScan:
         arp_request = ARP(pdst=ip)
         broadcast = Ether(dst='ff:ff:ff:ff:ff:ff')
         arp_request_broadcast = broadcast/arp_request
-        answered_list= srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+        answered_list = srp(arp_request_broadcast,timeout=1,verbose=False)[0]
         print("IP Address"+2*'\t'+'MAC Address'+ 3*'\t' + 'Company Name')
         print(75*"-")
         n=0
@@ -29,10 +28,9 @@ class arpScan:
             n+=1
         print('\n' + str(n) + ' Devices were discovered.' )
 
-    
     def start(self):
         ip = self.gateway+'/24'
         self.scan(ip)
 
-scanner = arpScan()
+scanner = Arpscan()
 scanner.start()
